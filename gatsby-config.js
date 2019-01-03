954212c970051e8e86a330dd509068a4d99801e2
Romain Lanz <romain.lanz@slynova.ch>
2019-01-03 21:13:33 +0100

    chore(analytics): add GA ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131756726-1`,
       },
     },
     `gatsby-plugin-feed`,
