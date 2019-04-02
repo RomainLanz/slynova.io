1a13c68504d9db33bf2ed71ec3703a5465537e94
Romain Lanz <romain.lanz@slynova.ch>
2019-04-02 23:41:51 +0200

    chore: fix indentation

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,9 +1,9 @@
 <template>
-	<g-link class="logo" to="/">
-	  <span class="logo__text">
-	    &larr; {{ $static.metaData.siteName }}
-	  </span>
-	</g-link>
+  <g-link class="logo" to="/">
+    <span class="logo__text">
+      &larr; {{ $static.metaData.siteName }}
+    </span>
+  </g-link>
 </template>
 
 <static-query>
@@ -16,16 +16,16 @@ query {
 
 <style lang="scss">
 .logo {
-	text-decoration: none;
+  text-decoration: none;
   color: var(--body-color)!important;
   font-size: .9em;
-  
-	&__image {
-		vertical-align: middle;
-		border-radius: 99px;
-		height: 40px;
-		width: 40px;
-		margin-right: .5em;
-	}	
+
+  &__image {
+    vertical-align: middle;
+    border-radius: 99px;
+    height: 40px;
+    width: 40px;
+    margin-right: .5em;
+  }
 }
 </style>
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -4,11 +4,11 @@
       <h1 class="post-title__text">
         {{ $page.post.title }}
       </h1>
-      
+
       <PostMeta :post="$page.post" />
 
     </div>
-    
+
     <div class="post content-box">
       <div class="post__header">
         <g-image alt="Cover image" v-if="$page.post.coverImage" :src="$page.post.coverImage" />
@@ -88,7 +88,7 @@ query Post ($path: String!) {
     margin-bottom: calc(var(--space) / 2);
     overflow: hidden;
     border-radius: var(--radius) var(--radius) 0 0;
-    
+
     img {
       width: 100%;
     }
@@ -119,7 +119,7 @@ query Post ($path: String!) {
 
 .post-comments {
   padding: calc(var(--space) / 2);
-  
+
   &:empty {
     display: none;
   }
